433027c244c090eb437c4136d00b9275533b84c6
damgau <damien.gauthier3003@gmail.com>
2016-05-30 16:16:02 +0200

    first

---
+++ Assets/Scripts/Javascript/Scenes/SceneGame.js
@@ -0,0 +1,97 @@
+/**
+ * Create a new Scene
+ * <ul><li>Copy the content of this file in a new .js document.</li>
+ * <li>Save the new file in Assets/Javascript/Scenes/NameOfYourScene.js .</li>
+ * <li>In the index.html add below this comment <!-- Scene --> the line: 
+*                    "<script type="text/javascript" src="Assets/Scripts/Javascript/Scenes/NameOfYourGameObject.js"></script>"</li>
+ * <li>For create a new scene, use this instruction: "new Scene()".</li>
+ * </ul>
+ * <strong>To load your scene, use this instruction: "Application.LoadLevel(LevelName)".</strong>
+ * 
+ * @class
+ * 
+ * @return {Scene}
+ * */
+function SceneGame() 
+{
+	this.name = "SceneGame";
+	this.GameObjects =[];
+	this.Groups = [];
+	this.Cameras = [];
+	this.CurrentCamera = null;
+	this.AlphaMask = null;
+	this.started = false;
+	this.score = 0;
+	this.WorldSize = new Vector(4096,4096);
+
+	/**
+	 * Called at the instruction new Scene().
+	 * */
+	this.Awake = function() 
+	{
+		console.clear();
+		Print('System:Scene ' + this.name + " Created !");
+	}
+	
+	/**
+	 * Start the Scene and show a message in console or launch Update() if already started
+	 * Called at the first use of scene in game.
+	 * */
+	this.Start = function() 
+	{
+		if (!this.started) 
+		{
+			Time.SetTimeWhenSceneBegin();
+			// operation start
+			var track = new Tracker();
+			var ball = new Ball();
+			this.GameObjects.push(track,ball);
+			this.started = true;
+			Print('System:Scene ' + this.name + " Started !");
+			Time.SetTimeWhenSceneLoaded();
+		}
+		this.Update();
+	}
+	/**
+	 * Start every GameObject, Group and apply the debug mode if asked
+	 * Called each frame,code game is here.
+	 * */
+	this.Update = function() 
+	{
+		if (!Application.GamePaused) 
+		{
+			for (var i = 0; i < this.GameObjects.length; i++) 
+			{
+				this.GameObjects[i].Start();
+			}
+			for (var i = 0; i < this.Groups.length; i++) 
+			{
+				this.Groups[i].Start();
+			}
+		}
+		if (Application.debugMode) 
+		{
+			Debug.DebugScene();
+		}
+		this.GUI();
+	}
+	/**
+	 * Called each frame, code all the GUI here.
+	 * */
+	this.GUI = function() 
+	{
+		if (!Application.GamePaused) 
+		{
+			//Show UI
+			ctx.fillStyle = "black";
+			ctx.font = "15px candara";
+			ctx.fillText("Score : " + this.score,10,canvas.height-20);
+		} 
+		else 
+		{
+			// Show pause menu
+		}
+	}
+
+	this.Awake()
+}
